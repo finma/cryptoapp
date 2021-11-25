360237907501703d69aca826fddb37dc40eda4ea
finma <firmanmaulana2403@gmail.com>
2021-11-25 13:38:30 +0700

    fix select button

--- src/component/Button/SelectButton.tsx
+++ src/component/Button/SelectButton.tsx
@@ -1,4 +1,10 @@
-export const SelectButton = ({ label, selected, onClick }) => {
+interface PropsTypes {
+  label: string;
+  selected: boolean;
+  onClick: () => void;
+}
+
+export const SelectButton = ({ label, selected, onClick }: PropsTypes) => {
   const clasess = `py-2 px-5 font-medium uppercase rounded-md border border-gold cursor-pointer ${
     selected ? "bg-gold text-black " : "bg-black-custom text-gold"
   }`;
