62f1c0a2511f3d019cf146a3406a0027a7150bd6
Firman Maulana <79675411+finma@users.noreply.github.com>
2021-11-27 09:26:42 +0700

    fix about page

--- src/pages/about/index.page.tsx
+++ src/pages/about/index.page.tsx
@@ -7,11 +7,11 @@ const About: CustomNextPage = () => {
   return (
     <main className="dark:bg-black-custom ">
       <Header />
-      <div className="w-full text-center flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-gold font-medium text-3xl">Cryptoverse</h1>
-        <div className="w-full lg:md-1/2 px-6">
-        <p className="text-base font-normal">
-          <span className="text-gold">Cryptoverse</span> adalah sebuah sistem informasi yang menyajikan data tentang cryptocurrency secara realtime, agar dapat membantu para pengguna cryptocurrency dalam melakukan kegiatannya.
+      <div className="w-full text-center flex flex-col justify-center min-h-screen">
+        <h1 className="text-gold font-medium text-5xl">Cryptoverse</h1>
+        <div className="w-full lg:w-1/2 px-6">
+        <p className="text-base font-normal text-white">
+          <span className="text-gold font-medium">Cryptoverse</span> adalah sebuah sistem informasi yang menyajikan data tentang cryptocurrency secara realtime, agar dapat membantu para pengguna cryptocurrency dalam melakukan kegiatannya.
         </p>
         </div>
       </div>
