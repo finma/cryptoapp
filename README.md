ad8683213aecca46c323b4b0839484188addf5aa
Firman Maulana <79675411+finma@users.noreply.github.com>
2021-11-27 09:14:51 +0700

    add about page

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# Next.js starter template.
+# Cryptoverse - Explore the World of Cryptocurrency
 
-- Next.js
-- TypeScript
-- ESLint
-- Prettier
-- Jest
-- Tailwind CSS
+<p align="center">
+    <img src="src/assets/img/screenshot1.png" >
+    <img src="src/assets/img/screenshot2.png" >
+    <img src="src/assets/img/screenshot3.png" >
+</p>
+
+## Tentang Cryptoverse 
+Cryptoverse adalah sebuah sistem informasi yang menyajikan data tentang cryptocurrency secara realtime, agar dapat membantu para pengguna cryptocurrency dalam melakukan kegiatannya.
+
+## Cryptocurrency
+Cryptocurrency adalah mata uang digital yang dijamin dengan kriptografi, yang membuatnya sulit untuk dipalsukan. Banyak cryptocurrency adalah jaringan terdesentralisasi berdasarkan teknologi blockchain (buku besar terdistribusi yang diberlakukan oleh jaringan komputer yang berbeda). ​
--- src/pages/about/index.page.tsx
+++ src/pages/about/index.page.tsx
@@ -1,10 +1,20 @@
 import type { CustomNextPage } from "next";
-import { FixedLayout } from "src/layout";
 
 const About: CustomNextPage = () => {
-  return <div>About</div>;
+  return (
+    <main className="dark:bg-black-custom ">
+      <Header />
+      <div className="w-full text-center flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-gold font-medium text-3xl">Cryptoverse</h1>
+        <div className="w-full lg:md-1/2 px-6">
+        <p className="text-base font-normal">
+          <span className="text-gold">Cryptoverse</span> adalah sebuah sistem informasi yang menyajikan data tentang cryptocurrency secara realtime, agar dapat membantu para pengguna cryptocurrency dalam melakukan kegiatannya.
+        </p>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
 };
-
-About.getLayout = FixedLayout;
 
 export default About;
