7cd77fe83d11c728c5a424af12cff3cbdf2b7590
Firman Maulana <79675411+finma@users.noreply.github.com>
2021-11-28 08:52:56 +0700

    Update index.page.tsx

--- src/pages/_app/index.page.tsx
+++ src/pages/_app/index.page.tsx
@@ -25,7 +25,7 @@ const App = (props: CustomAppProps) => {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
-        <title>nexst</title>
+        <title>Cryptoverse</title>
       </Head>
       <CryptoContext>{getLayout(<props.Component {...props.pageProps} />)}</CryptoContext>
     </>
